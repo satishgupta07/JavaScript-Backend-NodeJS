ff331a65c50a9c22ef685d3c18fd9956a23cfb1c
Satish Kumar Gupta <satishgupta.cse01@gmail.com>
2024-01-04 20:41:01 +0530

    all setup configured | controller | routes
    Signed-off-by: Satish Kumar Gupta <satishgupta.cse01@gmail.com>

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -2,7 +2,7 @@
     Wraps the provided requestHandler in a Promise, ensuring any errors are
     caught and passed to the next function.  */
 const asyncHandler = (requestHandler) => {
-  (req, res, next) => {
+  return (req, res, next) => {
     Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
   };
 };
