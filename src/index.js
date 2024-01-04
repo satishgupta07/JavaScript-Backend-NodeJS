03c69f266df23e75e5a2a1f60e53d54addc4aa2b
Satish Kumar Gupta <satishgupta.cse01@gmail.com>
2024-01-04 22:22:44 +0530

    Register Controller - logic building
    Signed-off-by: Satish Kumar Gupta <satishgupta.cse01@gmail.com>

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import connectDB from "./db/index.js";
 import { app } from "./app.js";
 
 dotenv.config({
-  path: "./env",
+  path: "./.env",
 });
 
 connectDB()
-.then(() => {
-app.listen(process.env.PORT || 8000, () => {
-    console.log(`⚙️   Server is running at port : ${process.env.PORT}`);
-});
-})
-.catch((err) => {
-console.log("MONGO db connection failed !!! ", err);
-});
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+  });
